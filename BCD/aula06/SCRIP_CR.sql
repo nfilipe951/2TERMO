da8ddeb9a96c3e5f114118d829f3dc0e6fb79442
nfilipe951 <ngg2903@gmail.com>
2026-09-10 10:38:21 -0300

    ex mysql

--- BCD/aula06/SCRIP_CR.sql
+++ BCD/aula06/SCRIP_CR.sql
@@ -26,3 +26,37 @@ INSERT into clinte (nome_cliente) VALUES
 ('Pedro'),
 ('Poste');
 
+
+CREATE Table fornecedor (
+    id_fornecedor int AUTO_INCREMENT PRIMARY KEY,
+    razao_social varchar (60) not null
+);
+
+create table profundo (
+    id_produto int AUTO_INCREMENT PRIMARY KEY,
+    nome_produto VARCHAR(50) not NULL,
+    id_fornecedor int not NULL,
+    Foreign Key (id_fornecedor) REFERENCES fornecedor (id_fornecedor)
+);
+
+
+-- DESAFIOS
+-- 1 - QUESTÃO
+-- CATEGORIA --- POSSUI --- PRODUTOS
+-- Uma categoria pode possuir vários produtos. Cada produto pertence a apenas uma categoria.
+
+-- 1. Uma categoria pode possuir vários produtos. Cada produto pertence a apenas uma categoria.
+-- (1,N)      PARA         (1,1)
+
+-- 2. Um funcionário pode registrar vários pedidos. Cada pedido é registrado por um funcionário.
+-- (1,N)      PARA         (1,1)
+
+-- 3. Um fornecedor comercializa vários produtos, e o mesmo produto pode ser comprado de vários fornecedores.
+-- (1,N)      PARA         (1,N)
+
+-- 4. Uma mesa pode existir sem nenhuma reserva futura. Uma reserva deve estar vinculada a uma mesa.
+-- (0,N)      PARA         (1,1)
+
+-- 5. Um pedido possui vários itens. Um item de pedido pertence a um único pedido.
+-- (1,1)      PARA         (1,N)
+
